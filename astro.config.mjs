1a59cdd4966fa6c68a2c327cd0cde53f1c593cc4
Eismer Lobaina <eismerlobaina@gmail.com>
2024-04-02 17:06:37 -0400

    installed pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), robotsTxt()],
   site: 'https://eismerlobaina.vercel.app/'
-})
+});
