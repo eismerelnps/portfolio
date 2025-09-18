7750d88661e7e8f2ea6bdb834887c9568488f012
Eismer Lobaina <eismerlobaina@gmail.com>
2025-09-18 12:38:36 -0400

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 import robotsTxt from "astro-robots-txt";
-import vercelServerless from '@astrojs/vercel/serverless';
-import vercelStatic from "@astrojs/vercel/static";
 
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercelStatic(),
-
-  // adapter: vercel({
-      //   analytics: true,
-      //   webAnalytics: {
-      //     enabled: true,
-      //   },
-      //   runtime: 'nodejs20.x',
-      //   mode: 'serverless'
-      // }),
+  output: 'server',
+  adapter: vercel({
+        analytics: true,
+        webAnalytics: {
+          enabled: true,
+        },
+        runtime: 'nodejs20.x',
+        mode: 'serverless'
+      }),
   integrations: [tailwind(), robotsTxt()],
   site: 'https://eismerlobaina.vercel.app/'
 });
