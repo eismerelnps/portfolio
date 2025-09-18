425686ae4fce041ffb774f9c33d74c2616157081
Eismer Lobaina <eismerlobaina@gmail.com>
2025-09-18 12:05:33 -0400

    Migrates to Vercel adapter and Node.js 18 runtime
    Updates the project to utilize the Vercel adapter for deployment and sets the Node.js runtime to version 18.
    
    This change ensures compatibility with Vercel's infrastructure and takes advantage of the latest Node.js features and security updates. It also enables web analytics.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,19 @@ import vercel from '@astrojs/vercel/serverless';
 import robotsTxt from "astro-robots-txt";
 
 
-import node from '@astrojs/node';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+      adapter: vercel({
+        analytics: true,
+        webAnalytics: {
+          enabled: true,
+        },
+        runtime: 'nodejs18.x',
+        mode: 'serverless'
+      }),
   integrations: [tailwind(), robotsTxt()],
   site: 'https://eismerlobaina.vercel.app/'
 });
